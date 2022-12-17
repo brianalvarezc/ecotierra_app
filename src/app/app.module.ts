8d32f6ee23d83301b8c0e3233f9f28e037d3a181
balvarez <balvarez@extreme.com.co>
2022-12-17 18:57:57 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LogosComponent } from './components/logos/logos.component';
+import { SesionComponent } from './components/sesion/sesion.component';
+import { IdiomaComponent } from './components/idioma/idioma.component';
+import { PuntosComponent } from './components/puntos/puntos.component';
+import { MapaComponent } from './components/mapa/mapa.component';
+import { ListaComponent } from './components/lista/lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogosComponent,
+    SesionComponent,
+    IdiomaComponent,
+    PuntosComponent,
+    MapaComponent,
+    ListaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/puntos/puntos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-puntos',
+  templateUrl: './puntos.component.html',
+  styleUrls: ['./puntos.component.css']
+})
+export class PuntosComponent {
+
+}
